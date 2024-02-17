79a753784e0cc2f08fe2a7adcb184593f41db6b9
Samuel Nascimento <101434230+samuelnasc-dev@users.noreply.github.com>
2024-02-16 22:34:22 -0300

    Delete midias-sociais/instagram/seguidores/teste.py

--- midias-sociais/instagram/seguidores/teste.py
+++
@@ -1,7 +0,0 @@
-nomes = ['benedomingo67', 'zxmorenin_', 'pvd._kelly9', 'saravxx58', 'duh_deto', 'elisangelacardosode.sousa', 'lucca_sartori08', 'luarahvxz', 'proficionais_unhas_', 'betocamargo455', 'orrapodcast', '7betdasorte', 'waraujo5', 'dgzim.085', 'vieiraa.julis', 'thayson_gb', 'almxd_nate', 'moreiraa.sosoo', 'welihh17', 'pgmimik', 'mariaa.isabellz', 'beatriz_vieiraa_s', '7777_k.k7']
-
-find_nome = 'benedomingo67' in nomes
-print(find_nome)
-
-find_index = nomes.index('benedomingo67')
-print(find_index)
